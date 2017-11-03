c54af71869cbfc83ff3b7fda703be35c634b962d
Henrique Luz Rodrigues <henrique.lr89@gmail.com>
2017-11-03 13:20:27 -0200

    fix issue #4 - add property unique in field

--- app/conf/populate_words.py
+++ app/conf/populate_words.py
@@ -1,28 +1,35 @@
 #!/usr/bin/env python
 #coding: utf-8
 
-import os, sys
-import csv
-import django
+import os, sys, csv, django
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings_production')
 django.setup()
 
+from django.db.utils import IntegrityError
 from conf import settings
 from main.models import Words
 
 
+
 class PopulateDB(object):
 	'''
-	import csv file and populate
-	base model in sqlite3
+	Import csv file and populate
+	base model Words.
 	'''
+	_total_errors_integrity = 0
+	_msg_error_integrity = str()
+
 	def __init__(self):
 		self.file_words = settings.FILE_PATH_WORDS
 
 	def insert_word(self, word):
-		return Words(word=word).save()
+		try:
+			return Words(word=word).save()
+		except IntegrityError as e:
+			self._total_errors_integrity += 1
+			self._msg_error_integrity = e.message
 
 	def import_csv_words(self):
 		with open(self.file_words, 'r') as f:
@@ -31,9 +38,10 @@ class PopulateDB(object):
 				self.insert_word(row['word'])
 		return True
 
+
 if __name__ == '__main__':
-	try:
-		PopulateDB().import_csv_words()
-	except Exception as Error:
-		print Error
-		pass
+	populate = PopulateDB()
+	populate.import_csv_words()
+	print ('Total de palavras repetidas: %s\nError: %s') % \
+			(populate._total_errors_integrity, populate._msg_error_integrity)
+
--- app/main/models.py
+++ app/main/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Words(models.Model):
 
 	id_words = models.AutoField(primary_key=True, verbose_name=u'words', db_column='id_words')
-	word = models.CharField(verbose_name=u'word', db_column='word', max_length=30)
+	word = models.CharField(verbose_name=u'word', db_column='word', max_length=30, unique=True)
 	updated_at = models.DateTimeField(verbose_name=u'Atualizado em', auto_now=True, db_column='updated_at')
 	created_at = models.DateTimeField(verbose_name=u'Criado em', auto_now_add=True, db_column='created_at')
 
