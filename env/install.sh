131d9312026b3391434e3211614ddc8ab4d45ffd
Henrique Luz Rodrigues <henrique.lr89@gmail.com>
2017-11-04 16:26:58 -0200

    Fix issue #5 - change paths project

--- env/install.sh
+++ env/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -xe
 
 sudo apt-get install -y openssh-server
-sudo ./env/config/sys/add-user.sh
+sudo ./env/sys/add-user.sh
 
 sudo apt-get install -y nginx
-sudo cp ./env/config/nginx/default /etc/nginx/sites-enabled/default
+sudo cp ./env/nginx/default /etc/nginx/sites-enabled/default
 
 sudo apt-get install -y curl wget
 sudo apt-get install -y python-pip python-dev build-essential freetds-dev
@@ -14,8 +14,7 @@ sudo apt-get install -y libaio1 psmisc libnuma1 libstdc++6 libjpeg-dev
 
 sudo pip install -r ./requirements.txt
 
-sudo cp ./env/config/supervisord/supervisord.conf /etc/supervisord.conf
-
+sudo cp ./env/supervisord/supervisord.conf /etc/supervisord.conf
 
 sudo ../app/manage.py collectstatic
 sudo find ../ -name "*.sqlite3" | xargs rm -f
